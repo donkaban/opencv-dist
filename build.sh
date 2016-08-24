d3c2a99fbc762f21a46dc50276b8ccb27afbbd0e
Kirill A. Shabordin <k.shabordin@cardsmobile.ru>
2016-08-24 14:26:00 +0300

    WiP

--- build.sh
+++ build.sh
@@ -1,49 +1,57 @@
 #!/usr/bin/env bash
 
-git submodule init
-git submodule update
-
 ROOT=`pwd`
-
+TAG='3.0.0'
 OPENCV_PATH=$ROOT/opencv
 BUILD_PATH=$ROOT/.build
 CONTRIB_PATH=$ROOT/opencv_contrib
 
-cd $OPENCV_PATH && git checkout . && git checkout tags/3.0.0
-cd $CONTRIB_PATH && git checkout . && git checkout tags/3.0.0
+git submodule init && git submodule update
+
+cd $OPENCV_PATH  && git checkout . && git checkout tags/$TAG
+cd $CONTRIB_PATH && git checkout . && git checkout tags/$TAG
 
 mkdir -p $BUILD_PATH
 cd $BUILD_PATH
 
-# cmake $OPENCV_PATH -G "Unix Makefiles" \
-#     -DWITH_GTK=0 \
-#     -DBUILD_DOCS=0 \
-#     -DBUILD_EXAMPLES=0 \
-#     -DBUILD_TBB=1 \
-#     -DOPENCV_EXTRA_MODULES_PATH=$CONTRIB_PATH/modules \
-#     -DBUILD_SHARED_LIBS=1 \
-#     -DWITH_OPENGL=0 \
-#     -DWITH_OPENCL=1 \
-#     -DWITH_VTK=0 \
-#     -DWITH_FFMPEG=1 \
-#     -DWITH_IPP=1 \
-#     -DWITH_CUDA=0 \
-#     -DWITH_OPENMP=1 \
-#     -DINSTALL_CREATE_DISTRIB=0 \
-#     -DINSTALL_C_EXAMPLES=0 \
-#     -DINSTALL_PYTHON_EXAMPLES=0 \
-#     -DINSTALL_TESTS=0 \
-#     -DOPENCV_WARNINGS_ARE_ERRORS=0 \
-#     -DBUILD_CUDA_STUBS=0 \
-#     -DWITH_EIGEN=1 \
-#     -DWITH_MATLAB=0 \
-#     -DCUDA_FAST_MATH=1 \
-#     -DWITH_CUBLAS=1 \
-#     -DWITH_EIGEN=1 \
-#     -DWITH_OPENNI=1 \
-#     -DWITH_OPENNI2=0 \
-#     -DWITH_WIN32UI=0 \
-#     -DCMAKE_BUILD_TYPE=Release
+cmake $OPENCV_PATH -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Release
+    -DOPENCV_WARNINGS_ARE_ERRORS=0 \
+    -DOPENCV_EXTRA_MODULES_PATH=$CONTRIB_PATH/modules \
+ \
+    -DINSTALL_CREATE_DISTRIB=1 \
+    -DINSTALL_C_EXAMPLES=0 \
+    -DINSTALL_PYTHON_EXAMPLES=0 \
+    -DINSTALL_TESTS=0 \
+  \
+    -DBUILD_DOCS=0 \
+    -DBUILD_EXAMPLES=0 \
+    -DBUILD_TBB=1 \
+    -DBUILD_SHARED_LIBS=1 \
+  \
+    -DWITH_GTK=0 \
+    -DWITH_TBB=0 \
+    -DWITH_QT=0 \
+    -DWITH_VTK=0 \
+    -DWITH_MATLAB=0 \
+    -DWITH_OPENGL=0 \
+    -DWITH_FFMPEG=0 \
+    -DWITH_OPENNI2=0 \
+    -DWITH_WIN32UI=0 \
+\
+    -DWITH_EIGEN=1 \
+    -DWITH_CUBLAS=1 \
+    -DWITH_OPENCL=1 \
+    -DWITH_IPP=1 \
+    -DWITH_OPENMP=1 \
+    -DWITH_OPENNI=1 \
+\
+    -DWITH_CUDA=0 \
+    -DBUILD_CUDA_STUBS=0 \
+    -DCUDA_FAST_MATH=1 \
 
-# make -j4 && sudo make install
+# make -j4 
+
+
+#sudo make install
 
--- prereq.sh
+++ prereq.sh
@@ -10,12 +10,12 @@ if [ $NAME == "Darwin" ]; then
     fi
     brew tap homebrew/science && brew update
     brew install git cmake automake autoconf libtool
-    brew install  jpeg libpng libtiff openexr eigen tbb
+    brew install  jpeg libpng libtiff openexr eigen 
     brew cleanup && brew prune
 fi
 
 if [ $NAME == "Linux" ]; then
     sudo apt-get install -y build-essential gcc g++ cmake git pkg-config python-dev python-numpy 
     sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
-    sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+    sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev libjasper-dev 
 fi
